6bdf913be87e6dc692a94ae658e455c31a05dc94
Ryosuke Otsuya <r.otsuya@gmail.com>
2013-11-24 09:55:52 +0900

    外部公開用にデザインモックアップ(/ui)を変更した。

--- js/ui.js
+++ js/ui.js
@@ -10,44 +10,36 @@
 var ui = {
     /**
      * ページを切り替えるメソッドです。
-     * @param pageName ページ名を 'list' か 'chat' で指定します。
+     * @param pageName ページ名を 'chat' か 'dial' で指定します。
      */
     showPage: function(pageName) {
         switch (pageName) {
-            case 'list':
-                $('html').removeClass('showing-chat-page').addClass('showing-list-page');
-                break;
             case 'chat':
-                $('html').removeClass('showing-list-page').addClass('showing-chat-page');
+                $('html').removeClass('showing-dial-page').addClass('showing-chat-page');
+                break;
+            case 'dial':
+                $('html').removeClass('showing-chat-page').addClass('showing-dial-page');
                 break;
         }
     },
 
     /**
      * モーダルダイアログを表示、非表示にするメソッドです。
-     * @param modalName モーダルの種類を 'login' か 'incoming' で指定します。
+     * @param modalName モーダルの種類を 'incoming' と指定します。
      * 引数なしで呼び出すとモーダルを非表示にします。
      */
     showModal: function(modalName) {
         switch (modalName) {
-            case 'login':
-                $('#loginModal').show();
-                // 同一スレッドで実行するとトランジションのアニメーションが描画されないので、
-                // 別スレッドで非同期に実行する。
-                setTimeout(function(){
-                    $('html').removeClass('showing-incoming-modal').addClass('showing-login-modal');
-                }, 0);
-                break;
             case 'incoming':
                 $('#incomingModal').show();
                 // 同一スレッドで実行するとトランジションのアニメーションが描画されないので、
                 // 別スレッドで非同期に実行する。
                 setTimeout(function(){
-                    $('html').removeClass('showing-login-modal').addClass('showing-incoming-modal');
+                    $('html').addClass('showing-incoming-modal');
                 }, 0);
                 break;
             default:
-                $('html').removeClass('showing-login-modal showing-incoming-modal');
+                $('html').removeClass('showing-incoming-modal');
                 // transform: translateZ() だけでモーダルを非表示にすると、<body> に overflow: hidden を
                 // 適用しているのにも関わらずスクロールできてしまうという、WebKitのバグが存在することがわかった。
                 // バグに対処するため、モーダルが非表示のときは .hide() を使って display: none を設定する。
@@ -70,13 +62,15 @@ var ui = {
     },
 
     /**
-     * 接続状態を変更するメソッドです。今のところ使っていません。
+     * 接続状態を変更するメソッドです。
      * @param state 状態を 'online' か 'offline' で指定します。
+     * @param yourId WebSocketサーバに接続した時の自分のIDを指定します。state が 'online' の時は必須。
      */
-    changeServiceState: function(state) {
+    changeServiceState: function(state, yourId) {
         switch (state) {
             case 'online':
                 $('html').removeClass('offline').addClass('online');
+                $('#yourId').text(ui._formatId(yourId));
                 break;
             case 'offline':
                 $('html').removeClass('online').addClass('offline');
@@ -97,6 +91,18 @@ var ui = {
                 $('html').removeClass('connected').addClass('disconnected');
                 break;
         }
+    },
+
+    /**
+     * IDの表示形式を、4桁ごとにスペースを入た、0000 0000 0000形式に整形します。
+     */
+    _formatId: function (id) {
+        id = id
+            .replace(/[^0-9]/g, '')
+            .slice(0, 12)
+            .replace(/([0-9]{4})/g, '$1 ')
+            .trimRight();
+        return id;
     }
 };
 
@@ -106,22 +112,18 @@ var ui = {
  */
 (function() {
     $(window).on('load', function() {
-        ui.showModal('login');
+        ui.scrollToBottom();
     });
+
+    setTimeout(function() {
+        // WebSocketに接続したら実行してください。
+        // ここでは setTimeout の中に書きます。
+        ui.changeServiceState('online', '123456789012');
+    }, 1000);
 
     // click イベントは、タッチデバイスで使用すると hold 等の判定のため 0.3 sec の遅延が入り、体感速度が低下する。
     // touchstart や touchend で代用すると体感速度が向上したが、スクロール時にタップを認識するなどの誤判定が発生した。
     // hammer.js ライブラリを導入し、tap イベントを使うことにした。
-    $('#userList')
-        .hammer({drag: false, hold: false, prevent_default: false, swipe: false, touch: false, transform: false})
-        .on('tap', 'li', function(event) {
-            var username = $(this).data('username');
-            console.log('リスト ' + username + ' がクリックされた');
-            // .scrollToBottom() → .showPage() の順だと、ページの一番下よりも少し上に
-            // スクロールしてしまう Chrome のバグを発見したため、順番を入れ替えた。
-            ui.showPage('chat');
-            ui.scrollToBottom();
-    });
 
     $('button')
         .hammer({drag: false, hold: false, prevent_default: false, swipe: false, touch: false, transform: false})
@@ -129,16 +131,45 @@ var ui = {
             var id = $(this).attr('id');
             console.log('ボタン ' + id + ' がクリックされた');
             switch (id) {
+                case 'dialButton':
+                    ui.showPage('dial');
+                    break;
                 case 'backButton':
-                    ui.showPage('list');
+                    ui.showPage('chat');
+                    ui.scrollToBottom();
                     break;
-                case 'cancelButton':
-                    ui.showModal('');
+                case 'backSpaceButton':
+                    var $anotherId = $('#anotherId');
+                    var id = $anotherId.val();
+                    id = ui._formatId(id.slice(0, -1));
+                    $anotherId.val(id);
                     break;
-                case 'okButton':
-                    ui.showModal('');
+                case 'callButton':
+                    ui.showPage('chat');
+                    ui.scrollToBottom();
+                    break;
+                case 'rejectButton':
+                    ui.showModal();
+                    break;
+                case 'acceptButton':
+                    ui.showModal();
+                    ui.showPage('chat');
+                    ui.scrollToBottom();
+                    break;
+                default:
+                    if ($(this).hasClass('number-button')) {
+                        var number = $(this).text();
+                        console.log(number);
+                        var $anotherId = $('#anotherId');
+                        $anotherId.val(ui._formatId($anotherId.val() + number));
+                    }
                     break;
             }
+    });
+
+    $('#anotherId').on('change', function() {
+        var $anotherId = $('#anotherId');
+        $anotherId.val(ui._formatId($anotherId.val()));
     });
 
     // body { overflow: hidden } を設定しているにもかかわらず、Chromeでスクロールできてしまう不具合に対処
@@ -146,5 +177,5 @@ var ui = {
         .on('scroll', function() {
             console.log('scroll to origin.')
             $(this).scrollTop(0).scrollLeft(0);
-        });
+    });
 })();
